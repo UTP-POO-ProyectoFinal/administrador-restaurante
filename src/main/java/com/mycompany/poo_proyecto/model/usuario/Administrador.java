58ac951bfb11e75680bd8fabb5e62ecf5d06f7fc
XimenaVentura-aj <venturaximenajim16@gmail.com>
2025-11-08 12:21:12 -0500

    Update Administrador.java

--- src/main/java/com/mycompany/poo_proyecto/model/usuario/Administrador.java
+++ src/main/java/com/mycompany/poo_proyecto/model/usuario/Administrador.java
@@ -3,23 +3,65 @@ package com.mycompany.poo_proyecto.model.usuario;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import java.util.List;
+import java.util.ArrayList;
 
 @Entity
 @Table(name = "administradores")
 public class Administrador extends Usuario {
+
     @Column(name = "nivel_acceso", nullable = false)
-    int nivelAcceso;
+    private int nivelAcceso;
+
     @Column(name = "area_responsable", nullable = false)
-    String areaResponsable;
-    
+    private String areaResponsable;
+
     public Administrador() {
-        
     }
-    
+
     public Administrador(int dni, String nombre, String apellido, String correo, String password, Roles rol, int nivelAcceso, String areaResponsable) {
         super(dni, nombre, apellido, correo, password, rol);
-        
         this.nivelAcceso = nivelAcceso;
+        this.areaResponsable = areaResponsable;
+    }
+
+    public boolean gestionarUsuarios() {
+        return true;
+    }
+
+    public List<Object> generarReportes() {
+        return new ArrayList<>();
+    }
+
+    public boolean gestionarInventario() {
+        return true;
+    }
+
+    public boolean registrarProveedor() {
+        return true;
+    }
+
+    public boolean registrarProducto() {
+        return true;
+    }
+
+    public boolean registrarPlatillo() {
+        return true;
+    }
+
+    public boolean registrarCompra() {
+        return true;
+    }
+
+    public int getNivelAcceso() {
+        return nivelAcceso;
+    }
+
+    public String getAreaResponsable() {
+        return areaResponsable;
+    }
+
+    public void setAreaResponsable(String areaResponsable) {
         this.areaResponsable = areaResponsable;
     }
 }
