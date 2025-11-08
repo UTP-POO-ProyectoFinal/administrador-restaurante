92e27458cd7a49fb16313f62dec872fd83f2e633
XimenaVentura-aj <venturaximenajim16@gmail.com>
2025-11-08 13:39:37 -0500

    Mesa.java

--- src/main/java/com/mycompany/poo_proyecto/model/reserva/Mesa.java
+++ src/main/java/com/mycompany/poo_proyecto/model/reserva/Mesa.java
@@ -1,13 +1,79 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.poo_proyecto.model.reserva;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 /**
- *
+ * 
  * @author Usuario
  */
+@Entity
+@Table(name = "mesas")
 public class Mesa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idMesa;
     
+    private int numero;
+    private int capacidad;
+    private String estado;
+    
+    public Mesa() {
+    }
+    
+    public Mesa(int numero, int capacidad, String estado) {
+        this.numero = numero;
+        this.capacidad = capacidad;
+        this.estado = estado;
+    }
+    
+    public void cambiarEstado(String nuevoEstado) {
+        this.estado = nuevoEstado;
+    }
+    
+    public int getIdMesa() {
+        return idMesa;
+    }
+
+    public void setIdMesa(int idMesa) {
+        this.idMesa = idMesa;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Mesa{" + 
+                "idMesa=" + idMesa + 
+                ", numero=" + numero + 
+                ", capacidad=" + capacidad + 
+                ", estado='" + estado + '\'' + 
+                '}';
+    }
 }
